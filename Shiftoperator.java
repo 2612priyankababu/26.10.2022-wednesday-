87bbc2f8ad764fbf3ce715bf09311a5bb3722845
2612priyankababu <116347966+2612priyankababu@users.noreply.github.com>
2022-10-26 12:13:14 +0530

    Add files via upload

---
+++ Shiftoperator.java
@@ -0,0 +1,11 @@
+package day3;
+
+public class Shiftoperator {
+
+	public static void main(String[] args) {
+		int num1=10,num2=5;
+		System.out.println(num1<<3);
+		System.out.println(num1>>2);
+	}
+
+}
