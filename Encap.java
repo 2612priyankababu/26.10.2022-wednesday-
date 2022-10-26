98f2043e85a1c92f545a00d048cb0693d9c9b6f4
2612priyankababu <116347966+2612priyankababu@users.noreply.github.com>
2022-10-26 13:45:07 +0530

    Add files via upload

---
+++ Encap.java
@@ -0,0 +1,29 @@
+package jjj;
+
+public class Encap {
+   private int a,b;
+   
+   public int getA() {
+	   return a;
+}
+   
+public void SetA(int a) {
+	this.a = a;
+}
+
+      public encap(int b) {
+      Super();
+      this.b = b;
+}      
+      public static void main(string[] args) {
+    	  // TODO Auto-generated method stub
+      encap e=new encap(7);
+      e.setA(8);
+      e.getA(9)
+      
+    	  
+      }
+
+}
+   
+
